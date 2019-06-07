3375eb37e22efd36bf5363e29d7f3249292479fa
hulloanson <hulloanson@gmail.com>
2019-06-07 13:43:52 +0800

    added vwheel factory

--- src/wheel.h
+++ src/wheel.h
@@ -1,10 +1,19 @@
 #ifndef VWHEEL_UDEV
 #define VWHEEL_UDEV
 
+#include <string.h>
+
 struct vwheel {
   int fd;
   char name[50];
 };
+
+struct vwheel* make_vwheel(const char *name) {
+  struct vwheel *wheel;
+  memset(wheel, 0, sizeof(struct vwheel));
+  strcpy(wheel->name, name);
+  return wheel;
+}
 
 int emit(struct vwheel *wheel, int type, int code, int val, int emit_syn);
 
