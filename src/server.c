a3ec7d468f0425d1982e2cccdcaa8e9612a9a3bb
hulloanson <hulloanson@gmail.com>
2019-06-07 13:38:26 +0800

    written what a server should do. not test yet

--- src/server.c
+++ src/server.c
@@ -10,60 +10,86 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <math.h>
 #include "lib.h"
+#include "server.h"
+#include "wheel.h"
 
-static int fd, pipe_r, pipe_w;
-
-static int close_fd() {
-  return close(fd);
+float get_float(char *bytes, int offset) {
+  float *value;
+  memset(value, 0, sizeof(float));
+  bcopy(bytes + offset, value, 4);
+  return *value;
 }
 
-int close_pipes() {
-  if (close(pipe_w) < 0) {
+signed int normalize_rotation(float raw, int raw_min, int raw_max) {
+  float amplified = raw * (raw_max - raw_min) + raw_min;
+  return (signed int) floorf(amplified);
+}
+
+struct frame* parse_data(char *bytes) {
+  struct frame *frame;
+  memset(frame, 0, sizeof(struct frame));
+  frame->wheel = normalize_rotation(get_float(bytes, WHEEL_OFFSET), WHEEL_MIN_VALUE, WHEEL_MAX_VALUE);
+  frame->gas = normalize_rotation(get_float(bytes, GAS_OFFSET), GAS_MIN_VALUE, GAS_MAX_VALUE);
+  frame->brake = normalize_rotation(get_float(bytes, BRAKE_OFFSET), BRAKE_MIN_VALUE, BRAKE_MAX_VALUE);
+  bcopy(bytes + BTNS_OFFSET, frame->btns, 40);
+  return frame;
+}
+
+int emit_frame(struct vwheel *wheel, struct frame *frame) {
+  if (emit(wheel, EV_ABS, ABS_WHEEL, frame->wheel, 1) < 0) {
     return -1;
   }
-  return close(pipe_r);
-}
-
-int serve(int pipe_r, int pipe_w) {
-  const int expected = 40;
-  int actual;
-  char buf[40];
-  char pipe_r_buf[10];
-  bzero(pipe_r_buf, 10);
-  while (1) {
-    bzero(buf, 40);
-    if (check_fail((actual = recvfrom(fd, buf, expected, 0, 0, 0)),
-                   "receiving data", close_fd, "close socket fd") < 0) {
+  if (emit(wheel, EV_ABS, ABS_GAS, frame->gas, 1) < 0){ 
+    return -1;
+  }
+  if (emit(wheel, EV_ABS, ABS_BRAKE, frame->brake, 1) < 0) {
+    return -1;
+  }
+  int i;
+  for (i = 0; i < BTNS_COUNT; i++) {
+    if (emit(wheel, EV_KEY, BTN_TRIGGER_HAPPY1 + i, frame->btns[i], 1) < 0) {
       return -1;
-    }
-    // Send buf over pipe_w
-    if (check_fail(write(pipe_w, buf, 40), "send received data over pipe", close_pipes, "close pipes") < 0) {
-      return -1;
-    }
-    if (check_fail(read(pipe_r, pipe_r_buf, 10), "read from parent", close_pipes, "close pipes")< 0) {
-      return -1;
-    }
-    if (strcmp("end", pipe_r_buf) == 0) {
-      break;
     }
   }
   return 0;
 }
 
-int server_setup() {
-  const int port = 20000;
+int close_server(struct server *srv) {
+  srv->should_run = 0;
+  return close(srv->fd);
+}
 
-  if (check_fail(fd = socket(AF_INET, SOCK_DGRAM, 0), "create udp socket", 0, 0) < 0) {
+int serve(struct server *srv, struct vwheel *wheel) {
+  // 3 axes, 40 buttons i.e. 3 * 4 bytes + 40 bytes = 52 bytes
+  const int expected_len = 52;
+  int actual_len;
+  char buf[expected_len];
+  while (srv->should_run) {
+    if (check_fail(recvfrom(srv->fd, buf, expected_len, 0, 0, 0), "receive data over UDP") < 0) {
+      close_server(srv);
+      return -1;
+    }
+    if (emit_frame(wheel, parse_data(buf)) < 0) {
+      close_server(srv);
+      return -1;
+    }
+  }
+  return 0;
+}
+
+int setup_server(struct server *srv) {
+  if (check_fail(srv->fd = socket(AF_INET, SOCK_DGRAM, 0), "create udp socket") < 0) {
     return -1;
   }
 
   struct sockaddr_in addr;
   memset(&addr, 0, sizeof(struct sockaddr_in));
   addr.sin_family = AF_INET;
-  addr.sin_port = htons(port);
+  addr.sin_port = htons(srv->port);
   addr.sin_addr.s_addr = htonl(INADDR_ANY);
-  if (check_fail(bind(fd, (struct sockaddr *)&addr, sizeof(struct sockaddr_in)), "bind to 0.0.0.0:20000", close_fd, "close socket fd") < 0) {
+  if (check_fail(bind(srv->fd, (struct sockaddr *)&addr, sizeof(struct sockaddr_in)), "bind to 0.0.0.0:20000") < 0) {
     return -1;
   }
   return 0;
--- src/server.h
+++ src/server.h
@@ -1,7 +1,36 @@
 #ifndef VWHEEL_SERVER
 #define VWHEEL_SERVER
 
+#define WHEEL_OFFSET 0
+#define GAS_OFFSET 4
+#define BRAKE_OFFSET 8
+#define BTNS_OFFSET 12
+#define BTNS_COUNT 40
 
-int serve(int pipe_r, int pipe_w);
+#define WHEEL_MAX_VALUE 180
+#define WHEEL_MIN_VALUE 0
+
+#define GAS_MIN_VALUE 0
+#define GAS_MAX_VALUE 200
+
+#define BRAKE_MIN_VALUE 0
+#define BRAKE_MAX_VALUE 200
+
+#include "wheel.h"
+
+struct server {
+  int port;
+  int fd;
+  int should_run;
+};
+
+struct frame {
+  signed int wheel;
+  signed int gas;
+  signed int brake;
+  char btns[40];
+};
+
+int serve(struct server *srv, struct vwheel *wheel);
 
 #endif
