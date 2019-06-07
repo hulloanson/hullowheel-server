c0d17139ce734e801b12dae4bfa4a89410c1edad
hulloanson <hulloanson@gmail.com>
2019-06-07 14:52:07 +0800

    also linked "m" (math)

--- src/main.c
+++ src/main.c
@@ -8,40 +8,58 @@
 #include <libexplain/ioctl.h>
 #include <limits.h>
 #include <pthread.h>
+#include <signal.h>
 #include "lib.h"
-#include "udev.h"
+#include "wheel.h"
 #include "server.h"
 
-// int run_commands() {
-//   char command = getchar();
-//   switch (command) {
-//   case 's':
-//     return emulate_press() < 0 ? -1 : 1;
-//   case EOF:
-//     fprintf(stderr, "Couldn't getchar(). wth.\n");
-//     return -1;
-//   case 'x':
-//     printf("Exiting.\n");
-//     return 0;
-//   default:
-//     return 1;
-//   }
-// }
+struct vwheel *wheel;
+struct server *srv;
 
+void* serve_in_thread(void *args) {
+  int *ret = (int*)args;
+  *ret = serve((struct server*) (args + 1), (struct vwheel*) (args + 2));
+  return ret;
+}
+
+void sig_handler(int sig) {
+  signal(sig, SIG_IGN);
+  printf("Interrupt detected. Exiting...");
+  close_server(srv);
+}
 
 // TODO: catch interrupt https://stackoverflow.com/questions/4217037/catch-ctrl-c-in-c
 int main(int argc, char **argv) {
-  if (setup() < 0) {
+  wheel = make_vwheel("HulloWheel");
+  if (setup_wheel(wheel) < 0) {
     return -1;
   }
 
-  char buf[1];
-  int run = 1;
-
-  pthread_t server;
-  if (pthread_create(&server, NULL, &serve, ))
-  while (run == 1) {
-    run = run_commands();
+  srv = make_server(20000);
+  if (setup_server(srv) < 0) {
+    return -1;
   }
-  return clean_up();
+
+  pthread_t server_thread;
+  void *args;
+  memset(args, 0, sizeof(void*) * 3);
+  int *ret;
+  void *ret_arg = args;
+  args = ret;
+  void *srv_arg = args + 1;
+  srv_arg = srv;
+  void *wheel_arg = args + 2;
+  wheel_arg = wheel;
+
+  int res = pthread_create(&server_thread, NULL, serve_in_thread, args);
+  if (res != 0) {
+    fprintf(stderr, "Could not create thread for server. error code was %d.\n", res);
+    return -1;
+  }
+
+  pthread_join(server_thread, NULL);
+
+  printf("Shutdown procedure completed. Bye.\n");
+
+  return -1;
 }
