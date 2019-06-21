4db0e38e53535fe7ed2f791ffa6ace927fbbe318
hulloanson <hulloanson@gmail.com>
2019-06-21 16:22:28 +0800

    used LOG_ERROR in check_fail

--- src/lib.c
+++ src/lib.c
@@ -1,9 +1,10 @@
 #include "lib.h"
 #include <stdio.h>
+#include "macrologger.h"
 
 int check_fail(int res, const char *what) {
   if (res < 0) {
-    fprintf(stderr, "Couldn't perform %s. Error code was %d\n", what, res);
+    LOG_ERROR("Couldn't perform %s. Error code was %d\n", what, res);
   }
   return res;
 }
